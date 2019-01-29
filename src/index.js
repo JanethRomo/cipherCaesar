b15e82db14c7aab62a64051de223b86644effd62
JanethRomo <janethromo72@gmail.com>
2019-01-29 13:33:55 -0600

    intentar cambios

---
+++ src/index.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    console.log('hola')
+location.replace('index2.html')
+}
+
+function myFunction1() {
+    console.log('hello')
+location.replace('file:///home/laboratoria/Escritorio/janeth/proyecto1/sws3.html')
+}
+
+//<script>
+//function myFunction() {
+//  location.replace("https://www.w3schools.com")
+//}
+//</script>
