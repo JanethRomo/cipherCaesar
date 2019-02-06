8bcbbd334976fa8be47d3077b0bfec1feb83631a
JanethRomo <janethromo72@gmail.com>
2019-02-06 12:39:25 -0600

    formula en cipher encode y decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,36 @@
 window.cipher = {
-  //cipher (String,offset =2);
-  //..
+  encode:function (offset, string){    
+    var palabraCifrada2 = '';
+    for (var i = 0; i < string.length; i++) {
+        if (string.charCodeAt(i) === 32) {
+            palabraCifrada2 += String.fromCharCode(32);
+        }
+        else {
+            // var numAscii = palabraCifrada.charCodeAt(i);
+            var letraCifrada = (string.charCodeAt(i) - 65 + offset) % 26 + 65;
+            palabraCifrada2 += String.fromCharCode(letraCifrada);
+        }
+    }
+   return palabraCifrada2;
+  },
+  decode:function (offset, string){    
+   var palabraCifrada2 = '';
+   for (var i = 0; i < string.length; i++) {
+       if (string.charCodeAt(i) === 32) {
+           palabraCifrada2 += String.fromCharCode(32);
+       }
+       else {
+           // var numAscii = palabraCifrada.charCodeAt(i);
+           var letraCifrada = (string.charCodeAt(i) +65 - offset) % 26 + 65;
+           palabraCifrada2 += String.fromCharCode(letraCifrada);
+       }
+   }
+  return palabraCifrada2;
+  }
+
+
+
 };
+ // Funcion para descifrar
+
+
--- src/index.js
+++ src/index.js
@@ -13,21 +13,21 @@ var string = ('A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z');
 var offset = 2;
 var palabraCifrada = "JANETH ROMO";
 
-function cifrarstring(palabraCifrada) {
-    var palabraCifrada2 = '';
-    for (var i = 0; i < palabraCifrada.length; i++) {
-        if (palabraCifrada.charCodeAt(i) === 32) {
-            palabraCifrada2 += String.fromCharCode(32);
-        }
-        else {
-            // var numAscii = palabraCifrada.charCodeAt(i);
-            var letraCifrada = (palabraCifrada.charCodeAt(i) - 65 + offset) % 26 + 65;
-            palabraCifrada2 += String.fromCharCode(letraCifrada);
-        }
-    }
-    return palabraCifrada2;
-}
-console.log(cifrarstring(palabraCifrada));
+// function cifrarstring(palabraCifrada) {
+//     var palabraCifrada2 = '';
+//     for (var i = 0; i < palabraCifrada.length; i++) {
+//         if (palabraCifrada.charCodeAt(i) === 32) {
+//             palabraCifrada2 += String.fromCharCode(32);
+//         }
+//         else {
+//             // var numAscii = palabraCifrada.charCodeAt(i);
+//             var letraCifrada = (palabraCifrada.charCodeAt(i) - 65 + offset) % 26 + 65;
+//             palabraCifrada2 += String.fromCharCode(letraCifrada);
+//         }
+//     }
+//     return palabraCifrada2;
+// }
+console.log(window.cipher.encode(offset,palabraCifrada));
 // Formula para descifrar
 /* var string = ('A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z');
 var offset = 2; */
